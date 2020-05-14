99d90b4b9c002873ffe33794033e10c74acc0361
wonseobshin <wonseob07@gmail.com>
2020-05-14 15:14:00 -0700

    Updates

---
+++ precache-manifest.c1ae3fe39e5a2d51beff687b55061de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f3751cc609fe6a337e7b899b49f210",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "97e458fdaf0e50660a4f",
+    "url": "/my-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b83bbf00f2e2d79e1d1",
+    "url": "/my-page/static/js/2.a1d79ca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-page/static/js/2.a1d79ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e458fdaf0e50660a4f",
+    "url": "/my-page/static/js/main.a337a341.chunk.js"
+  },
+  {
+    "revision": "0cbd4908dcc63d6e7f5a",
+    "url": "/my-page/static/js/runtime-main.7fa03308.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
